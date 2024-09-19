db824ac1d28626eb32131f9cd7492874a3e98e48
FernandoGutemberg <fernando70sil@gmail.com>
2024-09-19 17:38:32 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mariah',  // Substitua pelo nome do seu repositório
+  base: '/mariah/',  // Barras no início e no final
 })

bd3cebdeae5dfd461fe59ef646472e11550efce1
FernandoGutemberg <fernando70sil@gmail.com>
2024-09-19 17:50:52 -0300

    Subindo pronto

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mariah/',  // Barras no início e no final
-})
+  base: '/mariah',  // Barras no início e no final
+});

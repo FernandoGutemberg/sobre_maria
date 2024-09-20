7a2ae65b5072cc09865c5c44b8c3bb9bc188bb02
FernandoGutemberg <fernando70sil@gmail.com>
2024-09-20 10:46:39 -0300

    Corrige o nome do repositório no homepage e no base do Vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sobre_mariah/',  // Barras no início e no final
+  base: '/sobre_maria/',  
 });
